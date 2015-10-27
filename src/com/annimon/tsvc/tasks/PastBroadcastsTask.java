fd1423818059dceb92fbf58b30271a35174a4607
Victor <melnikukr@mail.ru>
2015-10-27 18:37:37 +0200

    Use javafx concurrent task

--- src/com/annimon/tsvc/tasks/PastBroadcastsTask.java
+++ src/com/annimon/tsvc/tasks/PastBroadcastsTask.java
@@ -2,7 +2,9 @@ package com.annimon.tsvc.tasks;
 
 import com.annimon.tsvc.Util;
 import com.annimon.tsvc.model.TwitchVideo;
+import java.util.ArrayList;
 import java.util.List;
+import javafx.concurrent.Task;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -10,21 +12,27 @@ import org.json.JSONObject;
  *
  * @author aNNiMON
  */
-public final class PastBroadcastsTask implements Task<String, List<TwitchVideo>> {
+public final class PastBroadcastsTask extends Task<List<TwitchVideo>> {
     
     private static final String VIDEOS_URL = "https://api.twitch.tv/kraken/channels/%s/videos?broadcasts=true";
+    
+    private final String channel;
 
+    public PastBroadcastsTask(String channel) {
+        this.channel = channel;
+    }
+    
     @Override
-    public List<TwitchVideo> process(String channel) throws Exception {
+    public List<TwitchVideo> call() throws Exception {
         final String jsonRaw = Util.getContent(String.format(VIDEOS_URL, channel));
         final JSONObject jsonBroadcasts = new JSONObject(jsonRaw);
         final JSONArray jsonVideos = jsonBroadcasts.getJSONArray("videos");
-        for (int i = 0; i < jsonVideos.length(); i++) {
-            TwitchVideo video = new TwitchVideo(jsonVideos.getJSONObject(i));
-            System.out.println(video);
+        final int length = jsonVideos.length();
+        final List<TwitchVideo> videos = new ArrayList<>(length);
+        for (int i = 0; i < length; i++) {
+            videos.add( new TwitchVideo(jsonVideos.getJSONObject(i)) );
         }
-        
-        return null;
+        return videos;
     }
 
 }
